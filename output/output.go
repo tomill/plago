a86cd01d4fabdc5cd5460d8b60c6182de6806fa0
Naoki Tomita <tomita@cpan.org>
2023-03-09 16:10:59 +0900

    dump dumper

--- output/output.go
+++ output/output.go
@@ -2,7 +2,9 @@ package output
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/tomill/centre/config"
 	"github.com/tomill/centre/message"
@@ -12,15 +14,31 @@ type Flusher interface {
 	Flush(message.Timeline) error
 }
 
-type Dump struct{}
-
-func DumpFlusher(config.Config) Flusher {
-	return &Dump{}
+type Dump struct {
+	json *json.Encoder
 }
 
-func (p Dump) Flush(timeline message.Timeline) error {
+func DumpFlusher(c config.Config) Flusher {
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetEscapeHTML(false)
 	enc.SetIndent("", "  ")
-	return enc.Encode(timeline)
+
+	fmt.Println("\n// config")
+	_ = enc.Encode(c)
+
+	if c.RawData != "" {
+		var d message.Timeline
+		_ = json.NewDecoder(strings.NewReader(c.RawData)).Decode(&d)
+		fmt.Println("\n// raw (decoded)")
+		_ = enc.Encode(d)
+	}
+
+	return &Dump{
+		json: enc,
+	}
+}
+
+func (p Dump) Flush(timeline message.Timeline) error {
+	fmt.Println("\n// timeline")
+	return p.json.Encode(timeline)
 }
